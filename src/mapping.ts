ff0c3412e852166af3caba0a3af0a80cd6d16ca2
Paul Spencer <paul@creativelayer.com>
2022-02-15 12:05:07 -0500

    Add Buy Event implementation

--- src/mapping.ts
+++ src/mapping.ts
@@ -13,7 +13,7 @@ import {
   SweepEvent,
   WithdrawEvent
 } from "../generated/RevenueSplitter/RevenueSplitter"
-import { Record, AccountRecord, Account, Collection, CollectionPayee } from "../generated/schema"
+import { Record, AccountRecord, Account, Collection, CollectionPayee , CollectionRecord } from "../generated/schema"
 
 import { Address, log } from '@graphprotocol/graph-ts'
 
@@ -62,7 +62,8 @@ export function handleAddPayeeEvent(event: AddPayeeEvent): void {
 
   record.save()
 
-  const accountRecordId = `${record.from}-${record.id}`
+  // link the record to the payee so it shows up as the account being added as a payee
+  const accountRecordId = `${payeeId}-${record.id}`
   let accountRecord = AccountRecord.load(accountRecordId)
   if (!accountRecord) {
     accountRecord = new AccountRecord(accountRecordId)
@@ -70,9 +71,69 @@ export function handleAddPayeeEvent(event: AddPayeeEvent): void {
     accountRecord.account = record.from
     accountRecord.save()
   }
+
+  // link the record to the collection so it shows up as the payee being added to the collection
+  const collectionRecordId = `${collectionId}-${record.id}`
+  let collectionRecord = CollectionRecord.load(collectionRecordId)
+  if (!collectionRecord) {
+    collectionRecord = new CollectionRecord(collectionRecordId)
+    collectionRecord.record = record.id
+    collectionRecord.collection = record.from
+    collectionRecord.save()
+  }
+
 }
 
-export function handleBuyEvent(event: BuyEvent): void {}
+export function handleBuyEvent(event: BuyEvent): void {
+  //emit BuyEvent(_collection, tokenId, msg.value);
+  const recordId = `${event.transaction.hash.toHex()}-${event.logIndex.toString()}`
+  let record = Record.load(recordId)
+  if (!record) {
+    record = new Record(recordId)
+  }
+  record.event = "Buy"
+
+  record.from = event.transaction.from.toHex()
+  let from = Account.load(record.from)
+  if (!from) {
+    from = new Account(record.from)
+    from.save()
+  }
+
+  if (event.transaction.to) { //always revenue share contract address
+    const accountId = (event.transaction.to as Address).toHexString()
+    if (!Account.load(accountId)) {
+      new Account(accountId).save()
+    }
+    record.to = accountId
+  }
+
+  // create or fetch collection
+  const collectionId = event.params.collection.toHex()
+  let collection = Collection.load(collectionId)
+  if (!collection) {
+    collection = new Collection(collectionId)
+    collection.save()
+  }
+  record.collection = collectionId
+
+  record.token = event.params.tokenId.toI32()
+  // event.params.amount == event.transaction.value
+  record.value = event.transaction.value
+  record.fee = event.transaction.gasPrice
+
+  record.save()
+
+  const collectionRecordId = `${record.from}-${record.id}`
+  let collectionRecord = CollectionRecord.load(collectionRecordId)
+  if (!collectionRecord) {
+    collectionRecord = new CollectionRecord(collectionRecordId)
+    collectionRecord.record = record.id
+    collectionRecord.collection = record.from
+    collectionRecord.save()
+  }
+
+}
 
 export function handleDepositEvent(event: DepositEvent): void {}
 
